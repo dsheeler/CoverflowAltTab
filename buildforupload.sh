04dff83623122477ff464a1b36570d8229bbd21d
Paolo Inaudi <p91paul@gmail.com>
2021-09-11 15:58:22 +0200

    Exclude .po files from upload archive

--- buildforupload.sh
+++ buildforupload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf CoverflowAltTab@palatis.blogspot.com.zip
 cd CoverflowAltTab@dmo60.de/
-zip -r ../CoverflowAltTab@palatis.blogspot.com.zip *
+zip -r ../CoverflowAltTab@palatis.blogspot.com.zip * -x '*.po'
 cd ../CoverflowAltTab@palatis.blogspot.com
 zip ../CoverflowAltTab@palatis.blogspot.com.zip metadata.json
