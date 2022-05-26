f9b5f6df026a87b868f2329f568ac4144c566886
Daniel Sheeler <dsheeler@pobox.com>
2022-05-26 15:33:00 -0500

    Exclude the .pot file from the build for upload

--- buildforupload.sh
+++ buildforupload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf CoverflowAltTab@palatis.blogspot.com.zip
 cd CoverflowAltTab@dmo60.de/
-zip -r ../CoverflowAltTab@palatis.blogspot.com.zip * -x '*.po'
+zip -r ../CoverflowAltTab@palatis.blogspot.com.zip * -x '*.pot' -x '*.po'
 cd ../CoverflowAltTab@palatis.blogspot.com
 zip ../CoverflowAltTab@palatis.blogspot.com.zip metadata.json
