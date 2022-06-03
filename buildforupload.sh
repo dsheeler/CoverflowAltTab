b2fd0afa139ccc263acfa834f348ffef46802251
Daniel Sheeler <dsheeler@pobox.com>
2022-06-03 15:06:06 -0500

    Exclude json schema from install

--- buildforupload.sh
+++ buildforupload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf CoverflowAltTab@palatis.blogspot.com.zip
 cd CoverflowAltTab@dmo60.de/
-zip -r ../CoverflowAltTab@palatis.blogspot.com.zip * -x '*.pot' -x '*.po'
+zip -r ../CoverflowAltTab@palatis.blogspot.com.zip * -x '*.pot' -x '*.po' -x 'settings-schema.json'
 cd ../CoverflowAltTab@palatis.blogspot.com
 zip ../CoverflowAltTab@palatis.blogspot.com.zip metadata.json
