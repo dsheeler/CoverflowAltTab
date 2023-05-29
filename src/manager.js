b71a62e662ca24f65713f76c2b6a4299f4a1b345
Daniel Sheeler <dsheeler@pobox.com>
2023-05-29 04:56:44 -0500

    Fix window sorting for "all workspaces, current first"

--- src/manager.js
+++ src/manager.js
@@ -101,6 +101,7 @@ var Manager = class Manager {
                 // Switch between windows of same application from all workspaces
                 let focused = display.focus_window ? display.focus_window : windows[0];
                 windows = windows.filter(matchWmClass, focused.get_wm_class());
+                windows.sort(sortWindowsByUserTime);
                 break;
 
             case 'switch-applications':
@@ -113,18 +114,19 @@ var Manager = class Manager {
               		// those from current workspace
               		let wins1 = windows.filter(matchWorkspace, currentWorkspace);
               		let wins2 = windows.filter(matchOtherWorkspace, currentWorkspace);
-                      // Sort by user time
-                      wins1.sort(sortWindowsByUserTime);
-                      wins2.sort(sortWindowsByUserTime);
-                      windows = wins1.concat(wins2);
-                      wins1 = [];
-                      wins2 = [];
+                    // Sort by user time
+                    wins1.sort(sortWindowsByUserTime);
+                    wins2.sort(sortWindowsByUserTime);
+                    windows = wins1.concat(wins2);
+                    wins1 = [];
+                    wins2 = [];
               	} else {
               	    let filter = currentOnly === 'current' ? matchWorkspace :
                           matchSkipTaskbar;
-                		// Switch between windows of current workspace
-                		windows = windows.filter(filter, currentWorkspace);
-              	}
+            		// Switch between windows of current workspace
+               		windows = windows.filter(filter, currentWorkspace);
+                    windows.sort(sortWindowsByUserTime);
+                }
                 break;
         }
 
@@ -134,9 +136,6 @@ var Manager = class Manager {
             windows = windows.filter ( (win) =>
               win.get_monitor() == Main.layoutManager.currentMonitor.index );
         }
-
-        // Sort by user time
-        windows.sort(sortWindowsByUserTime);
 
         if (windows.length) {
             let mask = binding.get_mask();
